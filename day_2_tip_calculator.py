c055148a81042fcc903511d6d33a0b308396454d
Reinkard <sergiy.khomiuk@gmail.com>
2023-03-06 21:09:27 +0200

    Add Day 2 Project: Tip Calculator

---
+++ day_2_tip_calculator.py
@@ -0,0 +1,13 @@
+print('Welcome to the tip calculator.')
+def pay(bill, percentage, people):
+    '''
+    Percentage to give + bill on peoples persons
+    '''
+    return (bill * (percentage / 100) + bill) / people
+
+bill = float(input('What is the total bill? $'))
+percentage = int(input('What percentage tip would you like to give? 10, 12 or 15? %'))
+while percentage != 10 and percentage != 12 and percentage != 15:
+    percentage = int(input('What percentage tip would you like to give? 10, 12 or 15? %'))
+people = int(input('How mant people to split the bill? '))
+print(round(pay(bill, percentage, people), 2))
