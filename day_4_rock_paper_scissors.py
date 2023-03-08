6ebde377a030aaacc2b5236e88a812df081ecbf3
Reinkard <sergiy.khomiuk@gmail.com>
2023-03-08 22:51:21 +0200

    Update Day 4 Project: Rock Paper Scissors

--- day_4_rock_paper_scissors.py
+++ day_4_rock_paper_scissors.py
@@ -1,10 +1,39 @@
 from random import randrange
 
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+
 win = 0
 lose = 0
 draw = 0
 
 text_play_again = 'Do you want to play again? Type y or n: '
+
+choose_picture = [rock, paper, scissors]
 
 play = True
 while play:
@@ -14,53 +43,29 @@ while play:
         player_choose = int(input('Type 0 for Rock, 1 for Paper or 2 for Scissors: '))
     # computer choose
     computer_choose = randrange(0, 3)
-    if computer_choose == 0:
-        computer_choose_print = '''
-        _______
-    ---'   ____)
-          (_____)
-          (_____)
-          (____)
-    ---.__(___)
-    '''
-    elif computer_choose == 1:
-        computer_choose_print = '''
-        _______
-    ---'   ____)____
-              ______)
-              _______)
-             _______)
-    ---.__________)
-    '''
-    elif computer_choose == 2:
-        computer_choose_print = '''
-        _______
-    ---'   ____)____
-              ______)
-           __________)
-          (____)
-    ---.__(___)
-    '''
     # draw
     if player_choose == computer_choose:
-        print(f'Computer choose {computer_choose}:')
-        print(computer_choose_print)
+        print(f'Computer choose: \n {choose_picture[computer_choose]}')
+        print(f'You choose: \n {choose_picture[player_choose]}')
+        print('It`s a draw!')
         draw += 1
-        play_again = input(f'Win: {win}, Lose: {lose}, Draw: {draw}, {text_play_again}')
+        play_again = input(f'Score: \nWin: {win}, Lose: {lose}, Draw: {draw} \n{text_play_again}')
     # win
     elif player_choose == computer_choose + 1 or computer_choose - player_choose == 2:
-        print(f'Computer choose {computer_choose}:')
-        print(computer_choose_print)
+        print(f'Computer choose: \n {choose_picture[computer_choose]}')
+        print(f'You choose: \n {choose_picture[player_choose]}')
+        print('You won!')
         win += 1
-        play_again = input(f'Win: {win}, Lose: {lose}, Draw: {draw}, {text_play_again}')
+        play_again = input(f'Score: \nWin: {win}, Lose: {lose}, Draw: {draw} \n{text_play_again}')
     # lose
     elif computer_choose == player_choose + 1 or player_choose - computer_choose == 2:
-        print(f'Computer choose {computer_choose}:')
-        print(computer_choose_print)
+        print(f'Computer choose: \n {choose_picture[computer_choose]}')
+        print(f'You choose: \n {choose_picture[player_choose]}')
+        print('You lose!')
         lose += 1
-        play_again = input(f'Win: {win}, Lose: {lose}, Draw: {draw}, {text_play_again}')
+        play_again = input(f'Score: \nWin: {win}, Lose: {lose}, Draw: {draw} \n{text_play_again}')
     # if play again is No
     if play_again == 'n':
         play = False
 
-print('Goodbye!')
+print(f'The final score is: \nWin: {win}, Lose: {lose}, Draw: {draw}\n Goodbye!')
