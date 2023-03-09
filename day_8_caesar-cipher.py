053bb639d77d65e73bf6df074d880e459600926f
Reinkard <sergiy.khomiuk@gmail.com>
2023-03-10 00:17:37 +0200

    Code optimization

--- day_8_caesar-cipher.py
+++ day_8_caesar-cipher.py
@@ -2,25 +2,17 @@ alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n'
 symbols = ['!', '@', '.', '/', ',', '&', '?', ' ']
 
 
-def encode(text, shift):
+def caesar(direction, text, shift):
+    if direction == 'decode':
+        shift *= -1
     result = ''
     for symbol in text:
         result += alphabet[abs((alphabet.index(symbol) + shift) % 26)] if symbol in alphabet else symbol
     return result
 
 
-def decode(text, shift):
-    result = ''
-    for symbol in text:
-        result += alphabet[abs((alphabet.index(symbol) - shift) % 26)] if symbol in alphabet else symbol
-    return result
-
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
-if direction == 'encode':
-    print('Result is: ', encode(text, shift))
-
-if direction == 'decode':
-    print('Result is: ', decode(text, shift))
+print('Result is: ', caesar(direction, text, shift))
