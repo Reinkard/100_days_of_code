fdcb2c3a8bc3feab174e4aa048f3a62ed1f940a9
Reinkard <sergiy.khomiuk@gmail.com>
2023-03-09 10:42:34 +0200

    New version of Day 5 Project: Password Generator

--- day_5_password_generator.py
+++ day_5_password_generator.py
@@ -1,25 +1,5 @@
 import random
 
-
-def check(pg_letters, pg_symbols, pg_numbers):
-    if pg_letters == 0:
-        if pg_numbers == 0:
-            return symbols
-        elif pg_symbols == 0:
-            return numbers
-        else:
-            return numbers + symbols
-    elif pg_numbers == 0:
-        if pg_symbols == 0:
-            return letters
-        else:
-            return symbols + letters
-    elif pg_symbols == 0:
-        return letters + numbers
-    else:
-        return letters + symbols + numbers
-    
-
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
@@ -28,17 +8,15 @@ print("Welcome to the PyPassword Generator!")
 pg_letters = int(input(f"How many letters would you like in your password?\n")) 
 pg_symbols = int(input(f"How many symbols would you like?\n"))
 pg_numbers = int(input(f"How many numbers would you like?\n"))
-result = ''
-while not (pg_letters == 0 and pg_numbers == 0 and pg_symbols == 0):
-    pg_password_table = check(pg_letters, pg_symbols, pg_numbers)
-    symbol = random.choice(pg_password_table)
-    if symbol in letters:
-        pg_letters -= 1
-    elif symbol in symbols:
-        pg_symbols -= 1
-    elif symbol in numbers:
-        pg_numbers -= 1
-    else:
-        continue
-    result += symbol
-print(result)
+pg_num_of_passwords = int(input("How many passwords would you like?\n"))
+for passwords in range(pg_num_of_passwords):
+    result = []
+    for let in range(pg_letters):
+        result.append(random.choice(letters))
+    for sym in range(pg_symbols):
+        result.append(random.choice(symbols))
+    for num in range(pg_numbers):
+        result.append(random.choice(numbers))
+    random.shuffle(result)
+    print(*result, sep='')
+ 
