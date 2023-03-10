d37c38f41fa38b26d165919cd0cfd072154d029f
Reinkard <sergiy.khomiuk@gmail.com>
2023-03-10 12:27:33 +0200

    Adding while loop

--- day_8_caesar-cipher.py
+++ day_8_caesar-cipher.py
@@ -11,8 +11,17 @@ def caesar(direction, text, shift):
     return result
 
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
-
-print('Result is: ', caesar(direction, text, shift))
+clipher = True
+while clipher:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+    while direction not in ['encode', 'decode']:
+        direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+    print('Result is: ', caesar(direction, text, shift))
+    play_again = input('Do you wat to decode/encode again?Type y(yes) or n(no): ')
+    while play_again not in ['y', 'n']:
+        play_again = input('Please type y(yes) or n(no): ')
+    if play_again == 'n':
+        clipher = False
+print('Have a nice day, good bye!')
