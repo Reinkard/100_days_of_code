a7d1914baf828e8de581d85b72f36e2f9eac72f9
Reinkard <sergiy.khomiuk@gmail.com>
2023-03-13 18:51:41 +0200

    little fixes

--- archive/day_12_the_number_guessing_game.py
+++ archive/day_12_the_number_guessing_game.py
@@ -13,6 +13,24 @@ def choose_difficulty():
     return lives
 
 
+def check_number(guess_num, lives):
+    while True:
+        print(f"You have {lives} attempts to guess the number.")
+        player_num = int(input("Make a guess: ")) # make a guess a number
+        if guess_num < player_num:
+            print("Too high!")
+        elif guess_num > player_num:
+            print("Too low!")
+        elif guess_num == player_num:
+            print(f"You won! It is {player_num}!")
+            break
+        if lives > 1: # if lives > 1 - player lose the one life
+                lives -= 1
+        else:
+            print("You lose!") # if lives = 0 - game over
+            break
+
+
 def make_number():
     return randint(1, 100)
 
@@ -20,32 +38,19 @@ def make_number():
 def game():
     print("Welcome to the Number Guessing Game!\n"
         "I`m thinking of a number between 1 and 100")
+    lives = choose_difficulty()         # choose difficulty
+    guess_number = make_number()        # create the random int number
+    check_number(guess_number, lives)
     while True:
-        lives = choose_difficulty()
-        number = make_number()
-        while True:
-            print(f"You have {lives} attempts to guess the number.")
-            guess_number = int(input("Make a guess: "))
-            if guess_number < number:
-                print("Too low!")
-            elif guess_number > number:
-                print("Too high!")
-            elif guess_number == number:
-                print(f"You won! It is {number}!")
-                break
-            if lives > 1:
-                lives -= 1
-            else:
-                print("You lose!")
-                break
-        while True:
-            user_choice = input("Do you want to play again? Type 'y' or 'n': ")
-            if user_choice in ['y', 'n']:
-                break
-        if user_choice == 'n':
-            print("Goodbye!")
-            input("Press Enter...")
+        user_choice = input("Do you want to play again? Type 'y' or 'n': ")
+        if user_choice in ['y', 'n']:
             break
+    if user_choice == 'n':
+        print("Goodbye!")
+        input("Press Enter...")
+        exit()
+    else:
+        game()
 
 
 game()
